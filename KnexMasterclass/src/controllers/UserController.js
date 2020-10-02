4e437a2eb61ea5ed5f0e7562b9cdbd233cab21d3
Marcus Vinicius <49821573+marcus-zuba@users.noreply.github.com>
2020-10-02 04:34:01 -0300

    Basic CRUD implementation

--- KnexMasterclass/src/controllers/UserController.js
+++ KnexMasterclass/src/controllers/UserController.js
@@ -1,8 +1,51 @@
+const { update } = require('../database')
 const knex = require('../database')
 
 module.exports = {
-  async index(req, res){
-    const results = await knex('users')
-    return res.json(results)
-  }
+   async index(req, res) {
+      const results = await knex('users')
+      return res.json(results)
+   },
+   async create(req, res, next) {
+      const { username } = req.body
+
+      try {
+         await knex('users').insert({
+            username
+         })
+
+         return res.status(201).send()
+      } catch (error) {
+         next(error)
+      }
+   },
+   async update(req, res, next){
+      try{
+         const { username } = req.body
+         const { id } = req.params
+
+         await knex('users')
+         .update({username})
+         .where({id})
+      
+         return res.send()
+      
+      }catch(error){
+         next(error)
+      }
+   },
+   async delete(req, res, next){
+      try{
+         const { id } = req.params
+
+         await knex('users')
+         .where({id})
+         .del()
+
+         return res.send()
+         
+      } catch(error){
+         next(error)
+      }
+   }
 }
